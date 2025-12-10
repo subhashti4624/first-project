67c40d19747d102c24049e921acceaa7385bb9c5
subhashti4624 <subhashti4624@gmail.com>
2025-12-10 11:18:55 +0530

    revert

---
+++ Python_projects/string.py
@@ -0,0 +1,20 @@
+str = "Hello world"
+str1 = 'hello world'
+str3 = """ Hello world"""
+
+print(str[1:6])
+#slicing method (positive)
+print(str[-6:-1])
+#Negative method 
+
+
+#string function    
+# sdgh
+title = "i am studying from Apna college"
+print(title.endswith("ege")) #return true if string ends with substr
+print(title.capitalize()) #Capitalize 1st character of sentence
+print(title.replace( "o", "a")) #Replace all occurance of old
+print(title.find("o")) # returns 1st index of 1st occurrer
+print(title.count("e")) # Counts the occurance of substr
+
+
